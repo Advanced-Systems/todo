f01e21fc700556c82534ff7d0bc2bbe590acb01a
StefanGreve <greve.stefan@outlook.jp>
2022-03-21 11:28:16 +0100

    Expand and rewrite examples

--- examples/examples.ps1
+++ examples/examples.ps1
@@ -1,18 +1,24 @@
 #
-# TodoList Recipes
+# Todo Module Examples
 #
 
-# 1 use the built-in filter parameter to perform simple searches
+# 1) Use the built-in filter parameter to perform simple searches
 gtodo -Filter Status -eq 'InProgress'
 
-# 2. review the description of the last task in list
+# 2) Review the description of the last task in list
 gtodo | select -Last 1 -Property Description
 
-# 3. dry-run mass removal of all discarded tasks
+# 3) Dry-run mass removal of all discarded tasks
 gtodo | where Status -eq 'Discarded' | Remove-Task -WhatIf
 
-# 4. get all high-priority tasks and sort by start date
+# 4) Get all high-priority tasks and sort by start date
 gtodo | where Priority -eq 'High' | sort StartDate | select Id, Description
 
-# 5. search all overdue task records that were never completed and update their status to discarded
+# 5) Search all overdue task records that were never completed and update their status to discarded
 gtodo | where { $(Get-Date) -gt $_.DueDate -and $_.Status -ne 'Done' } | utask -Status Discarded -WhatIf
+
+# 6) Display the description of task #42.
+gtask 10 | select Description
+
+# 7) Display the ID and description of all tasks
+gtodo -All | select Id, Description
