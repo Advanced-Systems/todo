6f37c97a4278645487deb130d360a2f226389a7c
StefanGreve <greve.stefan@outlook.jp>
2022-03-21 11:44:22 +0100

    Fix typo

--- examples/examples.ps1
+++ examples/examples.ps1
@@ -17,7 +17,7 @@ gtodo | where Priority -eq 'High' | sort StartDate | select Id, Description
 # 5) Search all overdue task records that were never completed and update their status to discarded
 gtodo | where { $(Get-Date) -gt $_.DueDate -and $_.Status -ne 'Done' } | utask -Status Discarded -WhatIf
 
-# 6) Display the description of task #42.
+# 6) Display the description of task #42
 gtask 10 | select Description
 
 # 7) Display the ID and description of all tasks
