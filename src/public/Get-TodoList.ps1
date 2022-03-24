946ce4c068fac6de29da26846f3f97197d330f2b
StefanGreve <greve.stefan@outlook.jp>
2022-03-24 12:45:56 +0100

    Declare OutputType as strings to fix a bug which breaks gtask and gtodo

--- src/public/Get-TodoList.ps1
+++ src/public/Get-TodoList.ps1
@@ -34,7 +34,7 @@ function Get-TodoList {
         List all tasks from the Work TODO list with a high priority.
     #>
     [Alias("gtodo")]
-    [OutputType([Task])]
+    [OutputType("Task")]
     [CmdletBinding()]
     param(
         [Parameter(ParameterSetName = "All")]
